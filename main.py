dc32a6905126546656c7f3ea32af5186720162d9
Stephen Froeber <stephen@srfaudio.com>
2023-05-15 23:17:10 -0500

    Added correct query

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ def main():
     """
     latitude = 38.59
     longitude = -89.91 # O'Fallon, IL
-    forecast = get_weather_forecast(latitude, longitude, hourly='temperature_2m', forecast_days=16)
+    hourly = 'temperature_2m,precipitation,windspeed_10m,winddirection_10m,windgusts_10m,is_day'
+    models = 'ecmwf_ifs04,gfs_seamless,jma_seamless,icon_seamless,gem_seamless,meteofrance_seamless'
+    daily = 'windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant'
+    windspeed_unit = 'kn'
+    timezone = 'America/Chicago'
+
+    forecast = get_weather_forecast(latitude, longitude, hourly=hourly, models=models, daily=daily, windspeed_unit=windspeed_unit, timezone=timezone)
     print(forecast)
 
     # Cache the forecast data
