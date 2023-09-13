2c67e6ac5ba39319a4557daa2a45273beafaa461
Stephen Froeber <srfaudioproductions@gmail.com>
2023-09-12 21:51:10 -0500

    Config changes

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,4 +1,7 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    DEBUG_MODE = os.environ.get('FLASK_DEBUG', 'False').lower() in ['true', '1']
+    HOST = os.environ.get('FLASK_HOST', '127.0.0.1')
+    app.run(debug=DEBUG_MODE, host=HOST)
